f3d8cdb04a6520f4d6327f6cf69e182b2e22c6b7
ancaza12 <56890947+ancaza12@users.noreply.github.com>
2019-11-26 11:51:53 -0500

    graf

--- ej30.py
+++ ej30.py
@@ -5,15 +5,12 @@ import matplotlib.pyplot as plt
 plt.figure()
 data = np.loadtxt('ej29.dat')
 
-plt.subplot(131)
-plt.imshow(data[:,0])
+plt.subplot(121)
+plt.plot(data[:,0], data[:,1])
 
-plt.subplot(132)
+plt.subplot(122)
 plt.plot(data[:,0], data[:,1])
 plt.xlabel('X')
 plt.ylabel('Y')
-
-plt.subplot(133)
-plt.plot(data[:,0], data[:,1])
 
 plt.savefig('difusion.png')
